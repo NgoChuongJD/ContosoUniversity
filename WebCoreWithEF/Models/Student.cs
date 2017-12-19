2dd6c92fc1fa6421da9b480e927ef1dfc2709341
euroland3 <euroland3@EUROLAND3-PC>
2017-12-19 14:59:39 +0700

    WebCoreCRUDwithEF

---
+++ WebCoreWithEF/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    [Table("StudentTB")]
+    public class Student
+    {
+         [Key]
+         public int ID { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
